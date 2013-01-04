6b80470823431a65457a5d14963919176ab69efa
ClemensDaCoder <madincubus@hotmail.com>
2013-01-04 19:00:47 +0100

    removed legacy code from board

--- src/pacman/model/Board.java
+++ src/pacman/model/Board.java
@@ -19,30 +19,12 @@ public class Board {
 			movingObject.setCurrentPosition(next);
 			nextField.addObject(movingObject);
 			currentField.removeObject(movingObject);
+			//TODO: object moved from current to next field .. event!
 		} else {
 			//TODO: object cannot move .. event!
 		}
-		
-//		if (movingObject instanceof PacMan) {
-//			moveObject((PacMan)movingObject, direction);
-//		} else if (movingObject instanceof Ghost) {
-//			moveObject((Ghost)movingObject, direction);
-//		}
 	}
 	
-//	private void moveObject(PacMan pacman, Direction direction) {
-//		//determine next Position of pacman
-//		  //get current position of pacman
-//		Position current = pacman.getCurrentPosition();
-//		Position next = current.next(direction);
-//		
-//		Field field = gameArea.get(next);
-//		//check if pacman can go to this field
-//		
-//	}
-//	
-//	private void moveObject(Ghost ghost, Direction direction) {
-//
-//	}
+	
 
 }
