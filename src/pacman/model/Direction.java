9fd31f9c0d4a7d6a592e752a595fd0fda1834de7
ClemensDaCoder <madincubus@hotmail.com>
2013-01-04 20:15:17 +0100

    added some javadoc

--- src/pacman/model/Direction.java
+++ src/pacman/model/Direction.java
@@ -1,4 +1,11 @@
 package pacman.model;
+
+import pacman.model.move.MovingObject;
+
+/** Represents directions availabe for moving {@link MovingObject}
+ * @author mwe
+ *
+ */
 public enum Direction {
 	RIGHT, LEFT, UP, DOWN
 }
