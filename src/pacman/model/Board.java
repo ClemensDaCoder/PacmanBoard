cef0ca52c402cf338b2ea21e4aae2741c886571e
Artur123 <Manuel@Manuel-PC.home>
2013-01-04 19:24:48 +0100

    Listener

--- src/pacman/model/Board.java
+++ src/pacman/model/Board.java
@@ -1,11 +1,27 @@
 package pacman.model;
+import java.util.EventListener;
 import java.util.HashMap;
 
+import pacman.model.bonus.BonusObject;
 import pacman.model.move.MovingObject;
 
 public class Board {
 
 	private HashMap<Position, Field> gameArea;
+	
+	private int score;
+	
+	public Board(){
+		resetScore();
+	}
+	
+	public void resetScore(){
+		score = 0;
+	}
+	
+	public void increaseScore(int points){
+		score += points;
+	}
 
 	public void moveObject(MovingObject movingObject, Direction direction) {
 		
@@ -25,6 +41,13 @@ public class Board {
 		}
 	}
 	
-	
+	public class BonusObjectEventListener implements EventListener{
+		public BonusObjectEventListener(){
+		}
+		
+		public void handleEvent(BonusObject o){
+			increaseScore(o.getValue());
+		}
+	};
 
 }
