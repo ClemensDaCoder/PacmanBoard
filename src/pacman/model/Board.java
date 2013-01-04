ea2e7b67299e48dfed9d331b29bad99b630d25c2
ClemensDaCoder <madincubus@hotmail.com>
2013-01-04 19:31:20 +0100

    merged usw

--- src/pacman/model/Board.java
+++ src/pacman/model/Board.java
@@ -1,58 +1,69 @@
 package pacman.model;
+
+import java.util.EventListener;
 import java.util.HashMap;
 
+import pacman.model.bonus.BonusObject;
 import pacman.model.move.MovingObject;
 
 public class Board {
 
-	private static Board instance = null; 
-	
+	private static Board instance = null;
+
 	private HashMap<Position, Field> gameArea;
-	
+
 	private int score;
-	
-	private Board()  {
+
+	private Board() {
 		score = 0;
 		init();
 	}
 
 	public void moveObject(MovingObject movingObject, Direction direction) {
-		
+
 		Position current = movingObject.getCurrentPosition();
 		Position next = current.next(direction);
-		
+
 		Field currentField = gameArea.get(current);
 		Field nextField = gameArea.get(next);
-		
+
 		if (!nextField.isWall()) {
 			movingObject.setCurrentPosition(next);
 			nextField.addObject(movingObject);
 			currentField.removeObject(movingObject);
-			//TODO: object moved from current to next field .. event!
+			// TODO: object moved from current to next field .. event!
 		} else {
-			//TODO: object cannot move .. event!
+			// TODO: object cannot move .. event!
 		}
 	}
-	
-	
+
 	public int getScore() {
 		return score;
 	}
-	
+
 	public void increaseScore(int value) {
 		score += value;
 	}
-	
+
 	public Board getInstance() {
 		if (instance == null) {
 			instance = new Board();
 		}
 		return instance;
 	}
-	
+
 	private void init() {
-		//TODO: initialize gameArea
+		// TODO: initialize gameArea
 	}
-	
+
+	//??
+	public class BonusObjectEventListener implements EventListener {
+		public BonusObjectEventListener() {
+		}
+
+		public void handleEvent(BonusObject o) {
+			increaseScore(o.getValue());
+		}
+	};
 
 }
