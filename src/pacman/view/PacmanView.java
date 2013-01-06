b832855053be865504a51aa85a99c737589cdac2
Artur123 <Manuel@Manuel-PC.home>
2013-01-06 20:28:50 +0100

    removed methods in PacmanView

--- src/pacman/view/PacmanView.java
+++ src/pacman/view/PacmanView.java
@@ -1,23 +1,9 @@
 package pacman.view;
 
-import java.util.HashMap;
 
 import pacman.event.BoardListener;
 import pacman.event.FieldListener;
-import pacman.model.Field;
-import pacman.model.Position;
 
 public interface PacmanView extends BoardListener, FieldListener{
-	/**
-	 * Prints the initial state of the gameboard
-	 * @param gameArea
-	 */
-	public void printBoard(HashMap<Position, Field> gameArea);
-	
-	/**
-	 * Prints the changed parts of the game
-	 * @param gameArea
-	 */
-	public void updateBoard();
-	
+
 }
--- src/pacman/view/TextView.java
+++ src/pacman/view/TextView.java
@@ -14,36 +14,6 @@ import pacman.model.Position;
 
 
 public class TextView implements PacmanView{
-	
-	@Override
-	public void printBoard(HashMap<Position, Field> gameArea){
-		// TODO Auto-generated method stub
-		
-	}
-	
-	@Override
-	public void updateBoard() {
-		// TODO Auto-generated method stub
-		
-		//pacman hits wall
-		
-		//ghost hits wall
-		
-		//pacman moves
-		
-		//ghost moves
-		
-		//ghost eats pacman: game over
-		
-		//pacman eats pellet
-		
-		//pacman eats fruit
-		
-		//score increased
-		
-		//level completed: next level
-		
-	}
 
 	@Override
 	public void hasmoved(HasmovedEvent e) {
