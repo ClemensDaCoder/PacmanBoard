8bccf5470d57f4b5ab15ed58f835b7de03c08085
ClemensDaCoder <madincubus@hotmail.com>
2013-01-06 23:02:19 +0100

    refactoring

--- src/pacman/event/ScoreChangedEvent.java
+++ src/pacman/event/ScoreChangedEvent.java
@@ -4,17 +4,17 @@ import java.util.EventObject;
 
 public class ScoreChangedEvent extends EventObject {
 
-	private int newscore;
+	private int newScore;
 
 	private static final long serialVersionUID = 1L;
 
-	public ScoreChangedEvent(Integer newscore) {
-		super(newscore);
-		this.newscore = newscore;
+	public ScoreChangedEvent(Integer newScore) {
+		super(newScore);
+		this.newScore = newScore;
 	}
 
 	public int getNewscore() {
-		return newscore;
+		return newScore;
 	}
 
 }
