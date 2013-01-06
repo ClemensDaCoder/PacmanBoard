778e47284aa20225ef1e721c15096b824caa7479
ClemensDaCoder <madincubus@hotmail.com>
2013-01-06 19:32:09 +0100

    cleanup

--- src/pacman/model/Board.java
+++ src/pacman/model/Board.java
@@ -1,4 +1,5 @@
 package pacman.model;
+
 import java.util.EventListener;
 import java.util.HashMap;
 
@@ -12,32 +13,32 @@ import pacman.event.ScoreChangedEvent;
 import pacman.model.bonus.BonusObject;
 import pacman.model.move.MovingObject;
 
-
 public class Board {
 
 	private static Board instance = null;
 
 	private HashMap<Position, Field> gameArea;
-	private EventListenerList listeners  = new EventListenerList();
+	private EventListenerList listeners = new EventListenerList();
 	private int score;
 
 	public Board() {
 		score = 0;
-		//init();
+		// init();
 	}
 
-	/** Moves a movingObject from its current Position to the next one.
-	 *  Objects are only moved from one {@link Field} to another.
-	 *  Objects are not moved if {@link Field} is occupied by {@link Wall}.
+	/**
+	 * Moves a movingObject from its current Position to the next one. Objects
+	 * are only moved from one {@link Field} to another. Objects are not moved
+	 * if {@link Field} is occupied by {@link Wall}.
+	 * 
 	 * @param movingObject
 	 * @param direction
 	 */
 	public void moveObject(MovingObject movingObject, Direction direction) {
-		
+
 		Position current = movingObject.getCurrentPosition();
 		Position next = current.next(direction);
 
-		
 		Field currentField = gameArea.get(current);
 		Field nextField = gameArea.get(next);
 
@@ -47,10 +48,9 @@ public class Board {
 			currentField.removeObject(movingObject);
 			notifyListener(new HasmovedEvent(current, next, movingObject));
 
-		}
-		 else {
-			 
-			 notifyListener(new MoveNotPossibleEvent(movingObject));
+		} else {
+
+			notifyListener(new MoveNotPossibleEvent(movingObject));
 		}
 	}
 
@@ -61,7 +61,9 @@ public class Board {
 		return score;
 	}
 
-	/** Increases the current score by value.
+	/**
+	 * Increases the current score by value.
+	 * 
 	 * @param value
 	 */
 	public void increaseScore(int value) {
@@ -79,158 +81,93 @@ public class Board {
 		return instance;
 	}
 
+	/** Initializes Board.
+	 * 
+	 */
 	public void init() {
 		gameArea = new HashMap<Position, Field>();
-		
-		/*for (Position position : Position.values()) {
-			Field f = new Field();
-			f.addObject(new Pellet());
-			gameArea.put(position, f);
-		}
-		PacMan pac = new PacMan("Pacman", Position.F_3);
-		moveObject(pac,Direction.RIGHT);*/
-		
-		// TODO: initialize gameArea
-		
-		for (Position position : Position.values()) {
-			/*f = new Field();
-			if(((position.getX() == 'A') || (position.getX() == 'Z')) && (position.getX() != 'M')){	
-				f.addObject(new Wall());
-			}else if(((position.getY() == 1)||(position.getY() == 35)) && (position.getY() != 17)){
-				f.addObject(new Wall());
-			}
-			gameArea.put(position, f);*/
-			
-			putWall(position, 'A', 'Z', 1, 35);
-			putWall(position, 'C', 'X', 3, 33);
-			putWall(position, 'E', 'U', 3, 33);
-			putWall(position, 'G', 'S', 3, 33);
-			putWall(position, 'I', 'Q', 3, 33);
-			putWall(position, 'K', 'O', 3, 33);
-		}
-		
-		/*for(Position position : Position.values()){
-			if((position.getX() == 17) && (position.getY() != 'A') && (position.getX() != 'Z')){
-				clearField(position);
-			}
-			else if((position.getY() == 'M') && (position.getX() != '1') && (position.getY() != '35')){
-				clearField(position);
-			}
-		}*/
+		initGameArea();
 	}
-	
-//	private void putSomething() {
-//		//put fields in all available positions
-//		for (Position position : Position.values()) {
-//			Field field = new Field();
-//			gameArea.put(position, field);
-//			if (position.getX() == 'A' || position.getX() == 'Z') {
-//				field.addObject(new Wall());
-//			} else if (field.isEmpty() && (position.getX() == 'C' || position.getX() == 'X') && (position.getY() >= 3 && position.getY() <= 33)) {
-//				field.addObject(new Wall());
-//			} else if (field.isEmpty() && (position.getX() == 'E'|| position.getX() == 'U') && (position.getY() >= 5 && position.getY() <= 31)) {
-//				field.addObject(new Wall());
-//			}
-//		}
-//		//put walls in fields of specific positions
-//		//in the a row
-//	}
-	
-	private void putWall(Position position, char X1, char X2, int Y1, int Y2){
-		Field f = new Field();
-		if(((position.getX() == X1) || (position.getX() == X2)) && (position.getX() != 'M')){	
-			f.addObject(new Wall());
-		}else if(((position.getY() == Y1)||(position.getY() == Y2)) && (position.getY() != 17)){
-			f.addObject(new Wall());
-		}
-		gameArea.put(position, f);
-	}
-	
-	/*private void putRectangle(Position pos, char X1, char X2, int Y1, int Y2){
-		Field f;
-		if((pos.getX() == X1) || (pos.getX() == X2)){
-			if ((pos.getX() != 17) || (pos.getY() != 'A')
-					|| (pos.getX() != 'Z')) {
-				f = new Field();
-				f.addObject(new Wall());
-				gameArea.put(pos, f);
-			}
-		}
-		
-		if(!gameArea.containsKey(pos)){
-			if((pos.getY() == Y1) || (pos.getY() == Y2)){
-				if ((pos.getY() != 'M') || (pos.getX() == 1)
-						|| (pos.getX() == 35)) {
-					f = new Field();
-					f.addObject(new Wall());
-					gameArea.put(pos, f);
-				}
-			}
-		}
-	}*/
 
-	//??
+	private void initGameArea() {
+		// TODO: initialize gameArea
+		Field f;
+		for (Position position : Position.values()) {
+			f = new Field();
+			if (f.isEmpty()) {
+				if ((position.getX() == 'A') || (position.getX() == 'Z') || (position.getY() == 1)
+						|| (position.getY() == 35)) {
+					f.addObject(new Wall());
+				} else if ((position.getY() % 2 == 1) && (Character.getNumericValue(position.getX()) % 2 == 0)) {
+					System.out.println(position.toString());
+					f.addObject(new Wall());
+				}
+				gameArea.put(position, f);
+			}
+		}
+	}
+	
+	//TODO: remove this, as it is only for testing purposes
+	public static void main(String[] args) {
+		Board board = Board.getInstance();
+		board.init();
+	}
+
 	public class BonusObjectEventListener implements EventListener {
 		public BonusObjectEventListener() {
 		}
-		
-		public void handleEvent(BonusObject o){
+
+		public void handleEvent(BonusObject o) {
 			increaseScore(o.getValue());
 		}
 	};
-	
-	public void addListener(BoardListener listener){
+
+	public void addListener(BoardListener listener) {
 		listeners.add(BoardListener.class, listener);
-		
+
 	}
-	public void removeListener(BoardListener listener){
+
+	public void removeListener(BoardListener listener) {
 		listeners.remove(BoardListener.class, listener);
-		
-		
+
 	}
-	protected void notifyListener(HasmovedEvent event){
+
+	protected void notifyListener(HasmovedEvent event) {
 		System.out.println("Notify");
-		for(BoardListener l : listeners.getListeners(BoardListener.class))
-		{
-			//System.out.println("Listerner");
+		for (BoardListener l : listeners.getListeners(BoardListener.class)) {
+			// System.out.println("Listerner");
 			l.hasmoved(event);
 		}
-		
-		
+
 	}
-	protected void notifyListener(MoveNotPossibleEvent event){
-		//System.out.println("Notify");
-		for(BoardListener l : listeners.getListeners(BoardListener.class))
-		{
-			//System.out.println("Listerner");
+
+	protected void notifyListener(MoveNotPossibleEvent event) {
+		// System.out.println("Notify");
+		for (BoardListener l : listeners.getListeners(BoardListener.class)) {
+			// System.out.println("Listerner");
 			l.moveNotpossible(event);
 		}
-		
-		
+
 	}
-	protected void notifyListener(ScoreChangedEvent event){
-		for(BoardListener l : listeners.getListeners(BoardListener.class))
-		{
-			//System.out.println("Listerner");
+
+	protected void notifyListener(ScoreChangedEvent event) {
+		for (BoardListener l : listeners.getListeners(BoardListener.class)) {
+			// System.out.println("Listerner");
 			l.scoreChanged(event);
 		}
 	}
 
-	public void addFieldListeners(FieldListener listener )
-	{
-		for(Field f : gameArea.values())
-		{
+	public void addFieldListeners(FieldListener listener) {
+		for (Field f : gameArea.values()) {
 			f.addListener(listener);
 		}
 	}
-	
-	public void removeFieldListener(FieldListener listener)
-	{
-		for(Field f : gameArea.values())
-		{
+
+	public void removeFieldListener(FieldListener listener) {
+		for (Field f : gameArea.values()) {
 			f.removeListener(listener);
 		}
-	
+
 	}
 
 }
