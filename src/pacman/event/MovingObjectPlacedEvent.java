8216abd2a24c229bf6872986dc788a7a7de14c8d
ClemensDaCoder <madincubus@hotmail.com>
2013-01-09 11:34:47 +0100

    code format

--- src/pacman/event/MovingObjectPlacedEvent.java
+++ src/pacman/event/MovingObjectPlacedEvent.java
@@ -5,21 +5,23 @@ import java.util.EventObject;
 import pacman.model.Position;
 import pacman.model.move.MovingObject;
 
-public class MovingObjectPlacedEvent extends EventObject{
+public class MovingObjectPlacedEvent extends EventObject {
 
 	private MovingObject mov;
 	private Position pos;
+
 	public MovingObjectPlacedEvent(MovingObject mov, Position pos) {
 		super(mov);
 		this.mov = mov;
 		this.pos = pos;
 	}
+
 	public MovingObject getMov() {
 		return mov;
 	}
-	
+
 	public Position getPos() {
 		return pos;
 	}
-	
+
 }
