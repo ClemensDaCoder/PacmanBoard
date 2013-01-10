9266bf0fd722a5c13bad91d57722f03786696623
ClemensDaCoder <madincubus@hotmail.com>
2013-01-10 13:47:02 +0100

    added javadoc

--- src/pacman/model/move/MovingStrategy.java
+++ src/pacman/model/move/MovingStrategy.java
@@ -2,8 +2,15 @@ package pacman.model.move;
 
 import pacman.model.Direction;
 
+/** Provides a move direction for moving objects.
+ *  Different implementations provide different moving strategies.
+ *  A special implementation would allow human input.
+ *  
+ * @author mwe
+ *
+ */
 public interface MovingStrategy {
 	
-	public  Direction getMoveDirection();
+	public Direction getMoveDirection();
 
 }
