ac78040cd3bec9710a31eb6579b7c453a8870664
ClemensDaCoder <madincubus@hotmail.com>
2013-01-20 13:59:21 +0100

    created first test class

---
+++ test/pacman/model/FieldTest.java
@@ -0,0 +1,22 @@
+package pacman.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FieldTest {
+	
+	private Field field;
+	
+	@Before
+	public void setUp() {
+		field = new Field();
+	}
+	
+	@Test
+	public void testIsWall() {
+		//TODO: here we will test if etc
+		
+//		assertTrue(field.isWall());
+	}
+
+}
