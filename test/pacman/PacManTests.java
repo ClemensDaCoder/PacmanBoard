5969c96aa4234378780a95e1d8235b865cf032b4
Artur123 <Manuel@Manuel-PC.home>
2013-01-20 15:40:58 +0100

    added MovingObjectTest to PacManTest

--- test/pacman/PacManTests.java
+++ test/pacman/PacManTests.java
@@ -7,9 +7,10 @@ import org.junit.runners.Suite.SuiteClasses;
 import pacman.model.FieldTest;
 import pacman.model.PositionTest;
 import pacman.model.bonus.BonusObjectTest;
+import pacman.model.move.MovingObjectTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({PositionTest.class, FieldTest.class, BonusObjectTest.class })
+@SuiteClasses({PositionTest.class, FieldTest.class, BonusObjectTest.class, MovingObjectTest.class })
 public class PacManTests {
 
 }
