fc12087d0a130a3db13eee0551218404bd7306d9
ClemensDaCoder <madincubus@hotmail.com>
2013-01-20 14:07:34 +0100

    added testWall & testEmpty

--- test/pacman/model/FieldTest.java
+++ test/pacman/model/FieldTest.java
@@ -1,5 +1,8 @@
 package pacman.model;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,16 +10,34 @@ public class FieldTest {
 	
 	private Field field;
 	
+	private Wall wall;
+	private GridObject gridObject2;
+	
 	@Before
 	public void setUp() {
 		field = new Field();
+		wall = new Wall();
 	}
 	
 	@Test
 	public void testIsWall() {
-		//TODO: here we will test if etc
-		
-//		assertTrue(field.isWall());
+		//initially, a field should not be a wall
+		assertFalse(field.isWall());
+		field.addObject(wall);
+		//now field should be a wall
+		assertTrue(field.isWall());
+		field.removeObject(wall);
+		//now field should not be a wall
+		assertFalse(field.isWall());
+	}
+	
+	@Test
+	public void testIsEmpty() {
+		assertTrue(field.isEmpty());
+		field.addObject(wall);
+		assertFalse(field.isEmpty());
+		field.removeObject(wall);
+		assertTrue(field.isEmpty());
 	}
 
 }
