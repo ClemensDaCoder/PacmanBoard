72b44c2f00ca556cfe0d4039e3e576685eab33ea
ClemensDaCoder <madincubus@hotmail.com>
2013-01-20 16:20:03 +0100

    code format

--- test/pacman/model/PositionTest.java
+++ test/pacman/model/PositionTest.java
@@ -40,7 +40,6 @@ public class PositionTest {
 	@Test
 	public void testNextDown() {
 		//tests if position down returns the wanted position
-
 		Position newPosition = position.next(Direction.DOWN);
 		assertEquals(nextDownPosition, newPosition);
 	}
@@ -48,7 +47,6 @@ public class PositionTest {
 	@Test
 	public void testNextLeft() {
 		//tests if position left returns the wanted position
-
 		Position newPosition = position.next(Direction.LEFT);
 		assertEquals(nextLeftPosition, newPosition);
 	}
@@ -56,18 +54,15 @@ public class PositionTest {
 	@Test
 	public void testNextRight() {
 		//tests if position right returns the wanted position
-
 		Position newPosition = position.next(Direction.RIGHT);
 		assertEquals(nextRightPosition, newPosition);
 	}
 
 	@Test
 	public void testInvalidPosition() {
-		// creating an invalid position is possible (please look at our
-		// "Fehlerprotokoll")
+		// creating an invalid position is possible (please look at our "Fehlerprotokoll")
 		Position invalidPosition = new Position('A', 45);
-		// moving into a direction that is not in the range of valid positions
-		// results in not moving at all
+		// moving into a direction that is not in the range of valid positions results in not moving at all
 		Position moreInvalidPosition = invalidPosition.next(Direction.DOWN);
 		assertEquals(invalidPosition, moreInvalidPosition);
 	}
