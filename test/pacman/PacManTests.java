07cabcd44f0569ec5e504a49d5cbd3ac8bd9533c
ClemensDaCoder <madincubus@hotmail.com>
2013-01-20 15:45:37 +0100

    added BoardTest to Suite

--- test/pacman/PacManTests.java
+++ test/pacman/PacManTests.java
@@ -4,12 +4,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import pacman.model.BoardTest;
 import pacman.model.FieldTest;
 import pacman.model.PositionTest;
 import pacman.model.bonus.BonusObjectTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({PositionTest.class, FieldTest.class, BonusObjectTest.class })
+@SuiteClasses({PositionTest.class, FieldTest.class, BonusObjectTest.class, BoardTest.class })
 public class PacManTests {
 
 }
